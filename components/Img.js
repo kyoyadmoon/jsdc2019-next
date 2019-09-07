dde90b8010c6bf4e1d8132d373734609c63bb61d
DMoon <dmoon.t@gmail.com>
2019-09-08 02:34:54 +0800

    Remove console.log

--- components/Img.js
+++ components/Img.js
@@ -1,5 +1,5 @@
 import { assetPrefix } from '~/constant/globalConfig';
-console.log('assetPrefix', assetPrefix);
+
 export default ({ src, ...props }) => (
   <img src={`${assetPrefix}${src}`} {...props}/>
 );
