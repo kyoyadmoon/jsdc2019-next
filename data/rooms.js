dbe3130384944a4b2e0add4e349a52481050b152
DMoon <dmoon.t@gmail.com>
2019-09-26 12:36:07 +0800

    Add data source of website

---
+++ data/rooms.js
@@ -0,0 +1,4 @@
+export default {
+  'day1':[ '' ],
+  'day2':[ 'MSpace' ],
+};
---
+++ data/team.js
@@ -0,0 +1,26 @@
+export default {
+  '議程組': [
+    { name:'維元 (Wei)', img: 'weiyuan' },
+    { name:'Zet', img: 'zet' },
+    { name:'Caesar', img: 'logo' },
+    { name:'Simon', img: 'simon' },
+  ],
+  '贊助組': [
+    { name:'Amy', img: 'amy' },
+    { name:'Kevin', img: 'logo' },
+    { name:'Deleav', img: 'logo' },
+  ],
+  '設計組': [
+    { name:'Nozomi', img: 'nozomi' },
+    { name:'Ashu', img: 'ashu' },
+    { name:'Maureen', img: 'maureen' },
+    { name:'ALi', img: 'ali' },
+  ],
+  '票務組': [
+    { name:'Amber', img: 'amber' },
+  ],
+  '場務組': [
+    { name:'Horken', img: 'horken' },
+    { name:'小馬', img: 'ma' },
+  ],
+};
